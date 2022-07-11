caa57ec717572c035bbb178c9514ada7da0e8efa
NezzNH <107748069+NezzNH@users.noreply.github.com>
2022-07-11 17:18:04 +0200

    Add files via upload
    This is an improved version! It includes rolling back letters. Before, it simply blindly added ASCII codes by some amount of X. Now, its a classic Caesar cipher that rolls back. Will add a function where it ignores punctuation, such as ; ! ? . , and such, but that's at a later date.

--- CeasarsCypher.c
+++ CeasarsCypher.c
@@ -9,6 +9,8 @@ int n=20;
 char currentChar;
 int shiftKey = 1;
 char actualCharacter;
+int tempKey;
+int modifier;
 printf("Please input the sentence or term you'd like to cipher: ");
 char letters[n];
 n = 0;
@@ -26,13 +28,34 @@ printf("\n");
 printf("Input the cipher key: ");
 scanf("%d", &shiftKey);
 
+if (shiftKey > 0){
+    modifier = 1;
+}
+
+else if (shiftKey < 0) {
+    modifier = -1;
+}
+
+else modifier = 0;
 printf("\n");
-for (i=1; i<=n; i++) {
+for (i=1; i<n; i++) {
 
 currentChar = letters[i];
 if (currentChar != 32) {
-    
-    actualCharacter = currentChar + shiftKey;
+    actualCharacter = currentChar;
+    for (tempKey = shiftKey; tempKey != 0; tempKey--) {
+        
+        if (actualCharacter > 122) {
+            actualCharacter = 98;
+        }
+        else if (actualCharacter > 90 && actualCharacter < 96) {
+            actualCharacter = 66;
+        }
+
+        else {
+            actualCharacter = actualCharacter + modifier;
+        }
+    }
     letters[i] = actualCharacter;
 
     printf("%c", letters[i]);
@@ -44,4 +67,6 @@ else printf(" ");
 
 printf("\n");
 
+printf("Press any key to exit:");
+getch();
 }
